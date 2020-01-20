e71c762fa983ca649e54335ab9bbb824fdb3ce43
KateAntropova-4415 <56113701+KateAntropova-4415@users.noreply.github.com>
2020-01-20 20:32:03 +0300

    .

--- KURSACH/django new/django_new/urls.py
+++ KURSACH/django new/django_new/urls.py
@@ -13,26 +13,14 @@ from django_new import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('hello', views.indexRender, name='indexRender'),
-    #path('ITMO', views.ITMO_University, name='ITMO'),
-    #path('discipline', views.disc, name='discipline'),
-    #path('group', views.group, name='group'),
-    #path('departaments', views.deps, name='departaments'),
-    #path('structureITMO', views.ITMO_structure, name='ITMOstructure'),
-    path('page1', views.page1, name='page1'),
     path('example', views.example, name='example'),
     path('', views.home, name='home'),
-    #path('page2', views.points, name='page2'),
     path('trains', views.trains, name='trains'),
     path('login/', views.login, name='login'),
     path('account/', views.account, name='account'),
     path('administ/', views.administ, name='administ'),
     path('moder/', views.moder, name='moder'),
     url('point/(?P<station_id>\d+)', views.point_trains, name='station_detail_url'),
-    #url('(?P<point_name>\d+)', views.point_dep, name='trains_dep'),
-    #url('(?P<point_name>\d+)', views.point_arr, name='trains_arr'),
-    #url('point_dep', views.point_dep, name='trains_dep'),
-    #url('point_arr', views.point_arr, name='trains_arr'),
     path('departure/', views.departure, name='departure'),
     path('arrival/', views.arrival, name='arrival'),
     path('adduser/', views.adduser, name='adduser'),
@@ -42,9 +30,9 @@ urlpatterns = [
     path('userlist/', views.userlist, name='userlist'),
     path('moderlist/', views.moderatorlist, name='moderatorlist'),
     path('stationlist/', views.stationlist, name='stationlist'),
+    path('stationlistuser/', views.stationlistuser, name='stationlistuser'),
     path('logout/', views.logout, name="logout"),
     #url(r'^town/(?P<station_id>\w{1,30})/$', views.point_trains_admin, name='station_detail_url_admin'),
     url(r'^stationlist/add-dep/(?P<station_id>\w{1,30})/$', views.addtripdep, name='addtripdep'),
     url(r'^stationlist/add-arr/(?P<id>\w{1,30})/$', views.addtriparr, name='addtriparr')
-    
 ]
--- KURSACH/django new/django_new/views.py
+++ KURSACH/django new/django_new/views.py
@@ -17,133 +17,18 @@ Loginglobal = "none"
 Passglobal = "none"
 Funcglobal = "none"
 
-
-
 with open("json_data1.json", encoding='utf-8') as read_file:
     data = json.load(read_file)
     stations = data['railway_station']
     dict_stations = {'railway_station': stations}
-        
 
-with open("json_data.json", 'r') as read_file:
-    data = json.load(read_file)
-    
-    name = data['name']
-
-    head_name = data['head']['name']
-    head_surname = data['head']['surname']
-
-    
-    index = data['location']['index']
-    city = data['location']['city']
-    adress = data['location']['adress']
-
-    
-    ad_amount = len(data['administrative'])
-
-    
-    sc_amount = len(data['scintific_and_educational'])
-
-    
-    mf_amount = 0
-    for i in data['scintific_and_educational']:
-        if i['name'].find("Мегафакультет") != -1:
-            mf_amount += 1
-
-    meg_id= data['scintific_and_educational'][0]['id']
-
-    meg_name=data['scintific_and_educational'][0]['name']
-
-    megITMO = {'meg_id': meg_id,
-               'meg_name':meg_name}
-
-    dep_amount = 0
-    for i in data['scintific_and_educational']:
-        dep_amount += len(i['departments'])
-
-
-    
-
-    edp_id = data['scintific_and_educational'][0]['departments'][0]['educational_programs'][0]['id']
-
-    
-    edp_name = data['scintific_and_educational'][0]['departments'][0]['educational_programs'][0]['name']
-
-    
-    dis_name = data['scintific_and_educational'][0]['departments'][0]['educational_programs'][0]['discipline']
-
-    
-    year_of_study = data['scintific_and_educational'][0]['departments'][0]['educational_programs'][0]['year']
-
-    
-    administrative = data['administrative']
-
-    
-    amount_of_groups = len(year_of_study['groups'])
-
-    
-    group = year_of_study['groups'][0]
-
-    
-    deps = data['scintific_and_educational'][0]['departments']
-
-
-    
-    dict_ITMO = {'name': name, 'head': [head_name, head_surname],
-            'adress': {'index': index, 'city': city, 'adress': adress},
-            'ad_amount': ad_amount,
-            'sc_amount': sc_amount,
-            'mf_amount': mf_amount,
-            'dep_amount': dep_amount}
-
-    
-    dict_disc = {
-                 'edp_id': edp_id,
-                 'edp_name': edp_name,
-                 'dis_name': dis_name,
-                 'year_of_study': year_of_study,
-                 'amount_of_groups': amount_of_groups}
-
-    
-    dict_group = {'group': group}
-
-    
-    dict_deps = {'departaments': deps}
-
-    
-    dict_uni = {'administrative': administrative}
-    dict_uni.update(megITMO)
- 
-    dict_uni.update(dict_deps)
-    
-    dict_uni.update(dict_ITMO)
-
-
-def index(request):
-    return HttpResponse("Hello, World!")
-def indexRender(request):
-    return render(request, 'index.html', {})
-def ITMO_University(request):
-    return render(request, 'universityInfo.html', dict_ITMO)
-def disc(request):
-    return render(request, 'disciplineInfo.html', dict_disc)
-def group(request):
-    return render(request, 'groupsInfo.html', dict_group)
-def deps(request):
-    return render(request, 'departamentsInfo.html', dict_deps)
-def ITMO_structure(request):
-    return render(request, 'universityStructure.html', dict_uni)
-def page1(request):
-    return render(request, 'Page1.html', {})
-def input(request):
-    return render(request, 'Input.html', {})
 def example(request):
     return render(request, 'Example.html', {})
 def home(request):
-    #with open("json_data1.json", encoding='utf-8') as read_file_json:
-     #   data = json.load(read_file_json)
-     #   stations = data['railway_station']
-     #   dict_stations = {'railway_station': stations}
+    with open("json_data1.json", encoding='utf-8') as read_file:
+        data = json.load(read_file)
+        stations = data['railway_station']
+        dict_stations = {'railway_station': stations}
     return render(request, 'HomePage.html', dict_stations)
 def page2(request):
     return render(request, 'page2.html', {})
@@ -181,7 +66,7 @@ def login(request):
     global Passglobal
     global Funcglobal
     #if 'id' in request:
-    #    return redirect("/account")
+    #return redirect("/account")
     if request.POST:
         with open("users.json", 'rb') as read_file_json:
             users = json.load(read_file_json)
@@ -196,7 +81,7 @@ def login(request):
                 Loginglobal = checkLogin
                 Passglobal = checkPass
                 Funcglobal = checkFunc
-                request.session.set_expiry(15)
+                request.session.set_expiry(8000)
                 request.session['id'] = user['id']
                 request.session['login'] = user['login']
                 request.session['position'] = user['position']
@@ -208,7 +93,7 @@ def login(request):
                     return redirect("/account")
 
     return render(request, 'login.html', {'form': logForm,
-                                          'error': error}) 
+                                          'error': error})
 
 def point_trains(request, station_id):
     with open("json_data1.json", encoding='utf-8') as read_file:
@@ -350,8 +235,6 @@ def adduser(request):
                 "login": checkLogin,
                 "id": ID,
                 "password": checkPass,
-                
-                #"Work": True,
                 "position": "User"
             }
             users.append(newuser)
@@ -496,12 +379,21 @@ def userlist(request):
 
 def stationlist(request):
     global Funcglobal
-    if Funcglobal == "none":
+    if Funcglobal == "none" or Funcglobal == "User":
         return redirect("/")
     with open("json_data1.json", encoding='utf-8') as read_file_json:
         data = json.load(read_file_json)
     Stations = data['railway_station']
     return render(request, "stationlist.html", {"Station": Stations, "Func": Funcglobal})
+
+def stationlistuser(request):
+    global Funcglobal
+    if Funcglobal == "none":
+        return redirect("/")
+    with open("json_data1.json", encoding='utf-8') as read_file_json:
+        data = json.load(read_file_json)
+    Stations = data['railway_station']
+    return render(request, "stationlistuser.html", {"Station": Stations, "Func": Funcglobal})
 
 def point_trains_admin(request, station_id):
     with open("json_data1.json", encoding='utf-8') as read_file:
@@ -528,5 +420,6 @@ def logout(request):
     Loginglobal = "none"
     Passglobal = "none"
     Funcglobal = "none"
+    request.session.clear()
     return redirect("/")
 
